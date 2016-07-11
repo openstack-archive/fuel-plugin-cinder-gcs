87577d279152cfd181b0cef6bb8dbf37c4a51d26
Taras Kostyuk <tkostyuk@mirantis.com>
2016-07-11 10:31:17 +0000

    Initial commit.
    The plugin structure added as generated by Fuel Plugin Builder.

---
+++ README.md
@@ -0,0 +1,4 @@
+fuel-plugin-cinder-gcs
+============
+
+Plugin description
---
+++ deployment_scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# It's a script which deploys your plugin
+echo fuel-plugin-cinder-gcs > /tmp/fuel-plugin-cinder-gcs
