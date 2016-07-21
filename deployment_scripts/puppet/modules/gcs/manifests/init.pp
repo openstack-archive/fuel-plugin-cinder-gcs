6d11a18dd086605d8479b675d4597bfe1942fdcc
Kostyantyn Kalynovskyi <kkalynovskyi@mirantis.com>
2016-07-21 17:19:09 +0300

    Added puppet manifests and plugin config files
    The uploaded files include puppet manifests to:
    - make changes to /etc/cinder/cinder.conf file on cinder nodes
    - install pip and googly-python-api-client packages
    Plugin config files:
    - environment_config.yaml adds variables for cinder backup configuration
    - deployment_tasks.yaml configures deployment tasks execution
    
    Change-Id: I515b597aac557414c3cd65219d4b286dc7df8a91

---
+++ deployment_scripts/puppet/modules/gcs/manifests/init.pp
@@ -0,0 +1,9 @@
+class gcs {
+  $services                 = 'cinder-backup'
+  $settings                 = hiera_hash('fuel-plugin-cinder-gcs')
+  $backup_driver            = 'cinder.backup.drivers.google'
+  $user_agent               = 'gcscinder'
+  $credential_file          = '/var/lib/cinder/credentials.json'
+  $pip_packages             = ['google-api-python-client']
+  $python_package_provider  = ['python-pip']
+}
