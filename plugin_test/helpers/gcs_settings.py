ec8b7dc13803bf6a936f58a0686cac1c7f4c3b4e
Vitalii Yerys <vyerys@mirantis.com>
2016-07-27 19:07:59 +0300

    Created base testing folder structure. Added fuel-qa folder as a submodule.
    Change-Id: I64932a41c3c65e8ec159b4918cb1cadd3acb7274

---
+++ plugin_test/helpers/gcs_settings.py
@@ -0,0 +1,26 @@
+#    Copyright 2016 Mirantis, Inc.
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Module with GCS plugin settings."""
+
+plugin_install_steps = {
+    'fuel_install': 'fuel plugins --install {rpm_name}'
+}
+
+rpm_name = 'fuel-plugin-cinder-gcs-1.0-1.0.0-1.noarch.rpm'
+plugin_name = 'fuel-plugin-cinder-gcs'
+
+default_tenant = 'gcs'
+default_user = 'gcs'
+default_user_pass = 'gcs123GCS'
