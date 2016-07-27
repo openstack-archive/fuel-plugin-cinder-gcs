d2f0710f472a0b4d2b3d0f2e8c7745ed02b98a3e
Vitalii Yerys <vyerys@mirantis.com>
2016-07-27 19:07:59 +0300

    Created base testing folder structure. Added fuel-qa folder as a submodule.
    Change-Id: I64932a41c3c65e8ec159b4918cb1cadd3acb7274

---
+++ plugin_test/helpers/gcs_settings.py
@@ -0,0 +1,52 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+tempest_cmds = {
+    'install_git': 'apt-get -y install git',
+    'clone_rally': 'git clone https://github.com/obutenko/mos-rally-verify.git',
+    'fixate_tempest_ver': "sed -i -re 's/(--version) [0-9a-fA-F]+/--version "
+                          "{tempest_commit}/' "
+                          "mos-rally-verify/install_tempest.sh",
+    'fix_docker_rally': "sed -i -re 's/(latest)/{rally_commit}/'"
+                        " /root/mos-rally-verify/prepare_env.sh",
+    'fix_ti': "sed -i -re 's/(exec -ti)/exec/'"
+              " /root/mos-rally-verify/prepare_env.sh",
+    'prepare_env': 'cd mos-rally-verify && ./prepare_env.sh',
+    'copy_report_to_master': 'scp {node_name}:/home/{report_name}.html .',
+    'test_list_path_on_lab': '/home/vyerys/test_list',
+    'docker_exec_command': 'docker exec {} {}',
+    'start_rally': 'rally verify start --tests-file {path_to_file}',
+    'verify_rally_results': 'rally verify results --html --output-file'
+                            ' {report_name}.html'
+}
+
+plugin_install_steps = {
+    'install_deps': 'yum install -y createrepo rpm rpm-build dpkg-devel'
+                    ' dpkg-dev git python-pip && pip install --upgrade pip',
+    'install_fpb': 'git clone https://github.com/stackforge/fuel-plugins.git '
+                   '&& cd fuel-plugins && sudo python setup.py install',
+    'clone_plugin_repo': 'git clone https://github.com/openstack/'
+                         'fuel-plugin-cinder-gcs.git',
+    'is_hotpluggable_fix': "sed -i -re 's/(is_hotpluggable: false)+/"
+                           "is_hotpluggable: true/' "
+                           "/root/fuel-plugin-cinder-gcs/metadata.yaml",
+    'build_plugin': 'fpb --build fuel-plugin-cinder-gcs',
+    'fuel_install': 'fuel plugins --install fuel-plugin-cinder-gcs/{rpm_name}'
+}
+
+path_to_tests_in_container = '/home/rally/test_list'
+tempest_commit = "63cb9a3718f394c9da8e0cc04b170ca2a8196ec2"
+rally_commit = 'latest'
+rpm_name = 'fuel-plugin-cinder-gcs-1.0-1.0.0-1.noarch.rpm'
+plugin_name = 'fuel-plugin-cinder-gcs'
---
+++ plugin_test/tests/test_gcs_basic.py
@@ -0,0 +1,106 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from proboscis.asserts import assert_equal
+from proboscis import test
+
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from helpers.gcs_base import GcsTestBase
+
+
+@test(groups=["test_gcs_all"])
+class GcsTestClass(GcsTestBase):
+    """GcsTestBase"""  # TODO documentation
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+          groups=["deploy_gcs_non_ha"])
+    @log_snapshot_after_test
+    def deploy_gcs_non_ha(self):
+        """
+        Deploy non HA cluster with GCS plugin installed and enabled.
+
+        Scenario:
+            1. Create cluster
+            2. Add 1 node with controller role
+            3. Add 1 node with compute role
+            4. Add 1 node with cinder role
+            5. Install GCS plugin
+            6. Deploy the cluster
+            7. Run network verification
+            8. Run OSTF
+        """
+        self.env.revert_snapshot("ready_with_3_slaves")
+        cluster_id = self.create_gcs_non_ha_cluster()
+
+        self.install_plugin()
+        self.set_attributes(cluster_id)
+
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+
+        cluster = self.fuel_web.client.get_cluster(cluster_id)
+        assert_equal(str(cluster['net_provider']), 'neutron')
+
+        self.fuel_web.verify_network(cluster_id)
+
+        self.fuel_web.security.verify_firewall(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            test_sets=['smoke', 'sanity', 'tests_platform', 'cloudvalidation'])
+        self.env.make_snapshot("deploy_gcs_non_ha")
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_5],
+          groups=["deploy_gcs_ha"])
+    @log_snapshot_after_test
+    def deploy_gcs_ha(self):
+        """
+        Deploy HA cluster with GCS plugin installed and enabled.
+
+        Scenario:
+            1. Create cluster
+            2. Add 3 node with controller role
+            3. Add 1 node with compute role
+            4. Add 1 node with cinder role
+            5. Install GCS plugin
+            6. Deploy the cluster
+            7. Run network verification
+            8. Run OSTF
+        """
+        self.env.revert_snapshot("ready_with_5_slaves")
+        cluster_id = self.create_env_gcs_ha()
+
+        self.install_plugin()
+        self.set_attributes(cluster_id)
+
+        self.fuel_web.deploy_cluster_wait(
+            cluster_id,
+            check_services=False
+        )
+
+        cluster = self.fuel_web.client.get_cluster(cluster_id)
+        assert_equal(str(cluster['net_provider']), 'neutron')
+
+        self.fuel_web.verify_network(cluster_id)
+
+        self.fuel_web.security.verify_firewall(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id,
+            test_sets=['smoke', 'sanity', 'ha', 'tests_platform',
+                       'cloudvalidation'])
+        self.env.make_snapshot("deploy_gcs_ha")
