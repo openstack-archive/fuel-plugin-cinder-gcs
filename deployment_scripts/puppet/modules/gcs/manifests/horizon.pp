6921645192bf2859968a0b7ebd8dd2a14ec7f983
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2016-09-01 21:26:20 +0300

    Class to enable cinder backups for Horizon added.
    Change-Id: Ifed6e3bd6a2b40a748bf1f8126c18d1da48da06b

---
+++ deployment_scripts/puppet/modules/gcs/manifests/horizon.pp
@@ -0,0 +1,29 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class gcs::horizon {
+
+  if $gcs::configure_horizon {
+	  file_line { 'configure_horizon':
+	    path  => '/etc/openstack-dashboard/local_settings.py',
+	    after => '^OPENSTACK_CINDER_FEATURES',
+	    match => '^\s*.enable_backup',
+	    line  => '"enable_backup": True,',
+	  } ~>
+	  service { 'apache2':
+	    ensure => running,
+	  }
+  }
+
+}
